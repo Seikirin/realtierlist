b84fb7142075bacb005b13af40ff17d0566d099b
Seikirin <81483357+Seikirin@users.noreply.github.com>
2022-12-31 19:56:07 +0100

    a

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let DataPromises = []
+const paramsArray = [];
 var cdata;
 
 async function fetchcdata(file) {
@@ -12,7 +13,9 @@ async function fetchcdata(file) {
 }
 
 DataPromises.push(new Promise(async function (resolve, reject) {
-	cdata = await fetchcdata(`data/azurlane.json`); resolve()
+	let str = localStorage.data ?? "azurlane"
+
+	cdata = await fetchcdata(`data/` + str + ".json"); resolve()
 }))
 
 await Promise.all(DataPromises)
@@ -20,6 +23,7 @@ await Promise.all(DataPromises)
 let items = document.querySelector(".items")
 let art = document.querySelector(".art")
 let tierlist = document.querySelector(".tierlist")
+let select = document.querySelector("#DataList")
 let selected = undefined;
 let itemsElms = [];
 let Tiers = [
@@ -50,6 +54,26 @@ let Tiers = [
 	},
 ];
 
+let DataSets = {
+	azurlane: "Azur Lane",
+	bluearchive: "Blue Archive",
+	arknights: "Arknights"
+}
+
+Object.keys(DataSets).forEach((key, i) => {
+	let option = document.createElement("option")
+	option.value = key;
+	option.textContent = DataSets[key];
+	select.appendChild(option);
+	if (localStorage.data == key)
+		select.selectedIndex = i;
+})
+select.addEventListener('change', (e) => {
+	console.log(e.target.selectedIndex)
+	localStorage.data = e.target.options[e.target.selectedIndex].value
+	location.reload();
+})
+
 function getOffset(el) {
 	const rect = el.getBoundingClientRect();
 	return {
@@ -59,11 +83,15 @@ function getOffset(el) {
 }
 
 function UpdateRemaining() {
-	var viewport_width = window.innerWidth;
-	art.setAttribute("style", "width: " + getOffset(tierlist).left + "px;")
+	console.log("changed")
+	art.setAttribute("style", "width: " + art.parentElement.clientWidth + "px;")
 }
 
-addEventListener("resize", UpdateRemaining);
+let observer = new ResizeObserver(function (entries) {
+	console.log("changed");
+	UpdateRemaining();
+});
+observer.observe(art.parentElement);
 UpdateRemaining();
 
 cdata.forEach((char, i) => {
@@ -124,7 +152,12 @@ function CreateTiers() {
 
 CreateTiers()
 
-
+function ApplyOnload(elem) {
+	elem.setAttribute("loaded", "false")
+	elem.onload = () => {
+		elem.setAttribute("loaded", "true")
+	}
+}
 
 function moveForward(arr, index, positions) {
 
@@ -143,7 +176,7 @@ function CancelSelection() {
 		elm.setAttribute("inactive", "false");
 	})
 	selected = undefined
-	art.querySelector("img").setAttribute("src", "")
+	art.innerHTML = "";
 }
 
 function IsAnItem(check) {
@@ -154,6 +187,15 @@ function IsAnItem(check) {
 			return (2);
 	})
 	return (0);
+}
+
+function CreateImage(url) {
+	let img = document.createElement("img");
+	img.src = url;
+	img.draggable = false;
+	art.scrollLeft = 0;
+	ApplyOnload(img);
+	return (img);
 }
 
 function HandleControl(elem) {
@@ -198,12 +240,11 @@ function HandleControl(elem) {
 			});
 		}
 	}
-	else if (elem.id == "add")
-	{
+	else if (elem.id == "add") {
 		if (elem.parentElement.className == "top") {
 			Tiers.some((Tier, i) => {
 				if (Tier.elm.contains(elem)) {
-					Tiers.splice(i, 0, {Title:"NEW", Color: "", Content: []});
+					Tiers.splice(i, 0, { Title: "NEW", Color: "", Content: [] });
 					CreateTiers();
 					return true;
 				}
@@ -213,7 +254,7 @@ function HandleControl(elem) {
 		else if (elem.parentElement.className == "bottom") {
 			Tiers.some((Tier, i) => {
 				if (Tier.elm.contains(elem)) {
-					Tiers.splice(i + 1, 0, {Title:"NEW", Color: "", Content: []});
+					Tiers.splice(i + 1, 0, { Title: "NEW", Color: "", Content: [] });
 					CreateTiers();
 					return true;
 				}
@@ -223,73 +264,78 @@ function HandleControl(elem) {
 	}
 }
 
+function ChangeSelection(event) {
+	itemsElms.forEach(elm => {
+		if (elm.querySelector("img") != event.target)
+			elm.setAttribute("inactive", "true");
+		else
+			elm.setAttribute("inactive", "false");
+	})
+	selected = event.target
+	art.innerHTML = ""
+	if (typeof cdata[event.target.parentElement.getAttribute("index")].Art == 'string')
+		art.appendChild(CreateImage(cdata[event.target.parentElement.getAttribute("index")].Art))
+	else {
+		cdata[event.target.parentElement.getAttribute("index")].Art.forEach(img => {
+			art.appendChild(CreateImage(img))
+		})
+	}
+}
+
+function AppendItem(event) {
+	if (tierlist.contains(event.target))
+		Tiers.forEach(Tier => {
+			if (Tier.elm.contains(event.target)) {
+				if (!IsAnItem(event.target.parentElement))
+					Tier.elm.querySelector(".tiercontent").appendChild(selected.parentElement)
+				else
+					Tier.elm.querySelector(".tiercontent").insertBefore(selected.parentElement, event.target.parentElement)
+			}
+		})
+	else
+		items.appendChild(selected.parentElement)
+}
+
 addEventListener('click', (event) => {
 	if (!event.target || !event.target.parentElement)
-		return ;
+		return;
 	if (event.target.parentElement.className &&
 		(event.target.parentElement.className == "top"
 			|| event.target.parentElement.className == "bottom")) {
 		HandleControl(event.target)
 		return;
 	}
-	if (IsAnItem(event.target.parentElement) && !selected) {
-		itemsElms.forEach(elm => {
-			if (elm.querySelector("img") != event.target)
-				elm.setAttribute("inactive", "true");
-			else
-				elm.setAttribute("inactive", "false");
-		})
-		selected = event.target
-		art.querySelector("img").setAttribute("src", "")
-		art.querySelector("img").setAttribute("src", cdata[event.target.parentElement.getAttribute("index")].Art)
-	}
-	else if ((tierlist.contains(event.target) || items.contains(event.target)) && selected) {
-		let tierlistcontains = tierlist.contains(event.target)
-		if (tierlist.contains(event.target))
-			Tiers.forEach(Tier => {
-				if (Tier.elm.contains(event.target)) {
-					if (!IsAnItem(event.target.parentElement))
-						Tier.elm.querySelector(".tiercontent").appendChild(selected.parentElement)
-					else
-						Tier.elm.querySelector(".tiercontent").insertBefore(selected.parentElement, event.target.parentElement)
-				}
-			})
-		else
-			items.appendChild(selected.parentElement)
-		CancelSelection()
-	}
+	console.log(event.target)
+	if (!selected && IsAnItem(event.target.parentElement))
+		ChangeSelection(event);
+	else if (IsAnItem(event.target.parentElement) && selected && items.contains(event.target) && items.contains(selected))
+		ChangeSelection(event);
+	else if (selected && (tierlist.contains(event.target) || items.contains(event.target)))
+		AppendItem(event),
+		CancelSelection();
 	else
-		CancelSelection()
-	return false;
-	if (itemsElms.includes(event.target.parentElement) && event.target.tagName == "IMG" && event.target != selected) {
-		itemsElms.forEach(elm => {
-			if (elm.querySelector("img") != event.target)
-				elm.setAttribute("inactive", "true");
-			else
-				elm.setAttribute("inactive", "false");
-		})
-		selected = event.target
-		art.querySelector("img").setAttribute("src", "")
-		art.querySelector("img").setAttribute("src", cdata[event.target.parentElement.getAttribute("index")].Art)
-	}
-	else if (tierlist.contains(event.target) && selected) {
-		Tiers.forEach(Tier => {
-			if (Tier.elm.contains(event.target)) {
-				items.removeChild(selected.parentElement)
-				Tier.elm.querySelector(".tiercontent").appendChild(selected.parentElement)
-			}
-		})
-		CancelSelection()
-	}
-	else if (selected && items.contains(event.target)) {
-		Tiers.forEach(Tier => {
-			if (Tier.elm.contains(selected)) {
-				Tier.elm.removeChild(selected.parentElement)
-				items.appendChild(selected.parentElement)
-			}
-		})
-		CancelSelection()
-	}
-	else
-		CancelSelection()
+		CancelSelection();
+	// if (tierlist.contains(event.target))
+	// {
+	// 	if (!selected)
+	// 		ChangeSelection()
+	// }
+
+	// if (IsAnItem(event.target.parentElement) && items.contains(event.target)
+	// 	&& (!selected || items.contains(selected) || tierlist.contains(event.target)))
+	// 	ChangeSelection(event)
+	// else if (selected && tierlist.contains(event.target))
+	// 	AppendItem(event),
+	// 		CancelSelection();
+	// else
+	// 	CancelSelection()
+	// if (IsAnItem(event.target.parentElement) && (!selected || items.contains(event.target))) {
+	// 	ChangeSelection(event)
+	// }
+	// else if ((tierlist.contains(event.target) || items.contains(event.target)) && selected) {
+	// 	AppendItem(event)
+	// 	CancelSelection()
+	// }
+	// else
+	// 	CancelSelection()
 })
